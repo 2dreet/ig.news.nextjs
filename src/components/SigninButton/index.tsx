da76717a0d9784b75a35830d1aa6c768c42531b2
José Augusto <jose.pasquali@snowmanlabs.com>
2021-10-23 12:34:22 -0300

    Next-auth com github

--- src/components/SigninButton/index.tsx
+++ src/components/SigninButton/index.tsx
@@ -1,25 +1,33 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+// aqui importamos a funcao de signIn que faz a autenticacao 
+// e o useSession que é um hook utilizado para obter varias informacao
+// sobra a sessao ate mesmo o usuario logado
+import { signIn, signOut,useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 export function SignInButton() {
 
-  const isUserLoggedIn = false;
+  // aqui obter a sessao caso nao tenha é undefined ou null
+  const [ session ] = useSession();
   // aqui é retornado um botão caso o usuario esteja logado
   // e retornado outro botão caso o usuario não esteja logado
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
-      José Augusto
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
     <button 
       type="button"
       className={styles.signInButton}
+      // aqui chamamos a funcao de logar passado qual regra de login vamos usar
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with Github
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
 import { AppProps } from 'next/app';
+// aqui utilizamos o provider do next-auth para compartilar se o usuario
+// esta logado ou nao
+import { Provider as NextAuthProvider } from 'next-auth/client';
 import {Header} from '../components/Header'
 
 // aqui importamos o style global
@@ -6,10 +9,10 @@ import '../styles/global.scss';
 
 function MyApp({ Component, pageProps } : AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   )
 }
 
